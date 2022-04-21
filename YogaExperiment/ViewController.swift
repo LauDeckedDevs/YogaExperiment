0bf0f705c91a04b076f3f7cead38077958711fb0
Laura Garcia <laudeckeddevs@gmail.com>
2022-04-21 02:34:22 +0200

    WIP - layout test

--- YogaExperiment/ViewController.swift
+++ YogaExperiment/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import YogaKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        guard let rootView = view else {
+            return
+        }
+        setUpUI(rootView: rootView)
     }
-
-
+    private func setUpUI(rootView: UIView) {
+        rootView.configureLayout { layout in
+            layout.isEnabled = true
+            layout.flexDirection = .column
+            layout.paddingTop = 80
+        }
+        let title = UILabel()
+        title.text = "Test"
+        title.textColor = .white
+        title.backgroundColor = .black
+        title.textAlignment = .center
+        title.configureLayout { layout in
+            layout.isEnabled = true
+            layout.width = 300
+            layout.height = 50
+            layout.alignSelf = .center
+        }
+        rootView.addSubview(title)
+        
+        let container = UIView()
+        container.layer.borderWidth = 1
+        container.layer.borderColor = UIColor.label.cgColor
+        container.configureLayout { layout in
+            layout.isEnabled = true
+            layout.flexDirection = .row
+            layout.justifyContent = .center
+            layout.height = 300
+            layout.width = YGValue(self.view.frame.size.width)
+        }
+        rootView.addSubview(container)
+        //Continue at 9:49
+        
+        rootView.yoga.applyLayout(preservingOrigin: false)
+    }
 }
-
